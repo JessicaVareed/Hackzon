1d0cc9cefaebf287909fb060c8218015c4849586
JessicaVareed <113986649+JessicaVareed@users.noreply.github.com>
2023-01-21 10:43:19 +0530

    Update lane.py
    Have added GUI part to this using Python built in library called Tkinter

--- lane.py
+++ lane.py
@@ -1,14 +1,13 @@
-
-
 import matplotlib.pylab as plt
 import cv2
 import numpy as np
-
+import tkinter
+import PIL.Image
+import PIL.ImageTk
 
 
 def region_mask(img, vert):
     mask=np.zeros_like(img)
-    #count=img.shape[2]
     match_colour=255
     
     cv2.fillPoly(mask, vert, match_colour)
@@ -30,7 +29,6 @@ def drawLines(img, lines):
 
 
 def process(image):
-    print(image.shape)
     height= image.shape[0]
     width= image.shape[1]
     
@@ -52,27 +50,147 @@ def process(image):
     imageLines=drawLines(image, line)
     return imageLines
 
-#image processing
 
-image =cv2.imread('lane.jpg')
-image= cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-f1=process(image)
-plt.imshow(f1)
-plt.show()
+def video():
+    cap= cv2.VideoCapture(v1)
+
+    frame_width =  int(cap.get(3))
+    frame_height = int(cap.get(4))
+    fourcc=cv2.VideoWriter_fourcc(*'mp4v')
+    out=cv2.VideoWriter('output_video3.mp4',fourcc, 20,
+                          (frame_width,frame_height))
+
+    while(cap.isOpened()):
+        ret, frame= cap.read()
+        if ret == True: 
+            frame= process(frame)      
+        
+            out.write(frame)
+        
+        else:
+            break
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
 
 
-#video processing
+class App:
+    def __init__(self, window, video_source1, video_source2):
+        self.window = window
+        self.window.title("Raod Lane Tracking System")
+        self.window.configure(bg="Thistle")
+        #self.window.l1=tkinter.Label()
+        #L1 = tkinter.Label(self.window, text="Input")
+        #L1.pack()
+        #L1.place(x=20, y=0, anchor='w')
+        self.video_source1 = video_source1
+        self.video_source2 = video_source2
+        self.photo1 = ""
+        self.photo2 = ""
 
-cap= cv2.VideoCapture('test_video.mp4')
+        # open video source
+        self.vid1 = MyVideoCapture(self.video_source1, self.video_source2)
 
-while(cap.isOpened()):
-    ret, frame= cap.read()
-    frame= process(frame)
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-cap.release()
-cv2.destroyAllWindows()
+        # Create a canvas that can fit the above video source size
+        self.canvas1 = tkinter.Canvas(window, width=500, height=500)
+        self.canvas2 = tkinter.Canvas(window, width=500, height=500)
+        self.canvas1.pack(padx=5, pady=10, side="left")
+        self.canvas2.pack(padx=5, pady=60, side="left")
+
+        self.delay = 15
+        self.update()
+
+        self.window.mainloop()
+
+    def update(self):
+        # Get a frame from the video source
+        ret1, frame1, ret2, frame2 = self.vid1.get_frame
+
+        if ret1 and ret2:
+                self.photo1 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame1))
+                self.photo2 = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame2))
+                self.canvas1.create_image(0, 0, image=self.photo1, anchor=tkinter.NW)
+                self.canvas2.create_image(0, 0, image=self.photo2, anchor=tkinter.NW)
+
+        self.window.after(self.delay, self.update)
+
+
+
+class MyVideoCapture:
+    def __init__(self, video_source1, video_source2):
+        # Open the video source
+        self.vid1 = cv2.VideoCapture(video_source1)
+        self.vid2 = cv2.VideoCapture(video_source2)
+
+        if not self.vid1.isOpened():
+            raise ValueError("Unable to open video source", video_source1)
+
+    @property
+    def get_frame(self):
+        ret1 = ""
+        ret2 = ""
+        if self.vid1.isOpened() and self.vid2.isOpened():
+            ret1, frame1 = self.vid1.read()
+            ret2, frame2 = self.vid2.read()
+            frame1 = cv2.resize(frame1, (500, 500))
+            frame2 = cv2.resize(frame2, (500, 500))
+            if ret1 and ret2:
+                # Return a boolean success flag and the current frame converted to BGR
+                return ret1, cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB),ret2, cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
+            else:
+                return ret1, None, ret2, None
+        else:
+            return ret1, None, ret2, None
+
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid1.isOpened():
+            self.vid1.release()
+        if self.vid2.isOpened():
+            self.vid2.release()
+
+
+def callback():
+    global v1,v2
+    v1=E1.get()
+    video()
+    v2='output_video3.mp4'
+    if v1 == "" or v2 == "":
+        L3.pack()
+        L3.place(x=425,y=330,anchor='center')
+        return
+    root.destroy()
+      
+
+v1 = ""
+v2 = ""
+
+root = tkinter.Tk()
+root.geometry('850x500')
+root.configure(bg="Thistle")
+root.title("Road Lane Tracking")
+lb=tkinter.Label(root, text="ROAD LANE TRACKING FOR SELF DRIVING CARS", 
+                 font=("Arial", 25),
+                 justify=tkinter.CENTER)
+lb.pack()
+lb.place(x=425,y=100,anchor='center')
+L1 = tkinter.Label(root, text="Video name")
+L1.pack()
+L1.place(x=425,y=225, anchor='center')
+E1 = tkinter.Entry(root, bd =5)
+E1.pack()
+E1.place(x=425, y=260, anchor='center')
+B = tkinter.Button(root, text ="Next",
+                   bg="SlateBlue", 
+                   command = callback)
+B.pack()
+B.place(x=425,y=300, anchor='center')
+L3 = tkinter.Label(root, text="Enter the name of the video")
+
+root.mainloop()
+
+
+App(tkinter.Tk(),v1,v2)
+
     
